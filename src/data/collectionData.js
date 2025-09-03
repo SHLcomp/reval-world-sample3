cb3622736c366ff8ef767d0ef42f03752379a31f
Eng. Sarah Seenne <sb12321b@gmail.com>
2025-09-03 03:19:10 +0300

    fixes

--- src/data/collectionData.js
+++ src/data/collectionData.js
@@ -2,6 +2,7 @@ import img1 from '../assets/bg4.jpg' //first one
 import img2 from '../assets/bg5.jpg' //2nd one
 import img3 from '../assets/bg1.png' //before last
 import img4 from '../assets/bg6.jpg' //last one water
+// import img5 from '../assets/bg6.jpg' //last one water
 const collection = [
     {
         name: "Automatic Doors",
@@ -26,6 +27,12 @@ const collection = [
         name: "Automatic Doors",
         path: "/autoDoors",
         img: img4,
+        className: "box"
+    },
+    {
+        name: "Automatic Doors",
+        path: "/autoDoors",
+        img: img2,
         className: "box"
     }
 
